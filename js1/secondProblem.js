c7bae9ff0bc1549aa6d034f77e6a4f83159cb397
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-05-08 22:10:26 -0700

    Storing numbers and strings in variables #2

---
+++ js1/secondProblem.js
@@ -0,0 +1,10 @@
+// Store numbers and strings in variables #2
+
+//1. Create and store the number 42 in a variable.
+    let numStoring = 42;
+
+//2. Create and store the word "forty-two" in a variable.
+    let wordStoring = "forty-two";
+
+//3. Are const index = '42'; and const index = 42; the same? Why or why not?
+    //they are not the same because the first one is an interger(numbers) and the second is a string 42(letters).
