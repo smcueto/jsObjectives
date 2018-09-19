e5828f7e79949b7499aeac40d402c8d41926b4f4
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-09-18 19:21:04 -0700

    Create nested arrays #9

---
+++ js2/09.js
@@ -0,0 +1,10 @@
+//After each step, log the value or question / answer.
+
+//Create three consts containing arrays: alpha which is empty, beta which contains three integers of your choice,
+// and delta which contains two strings of your choice.
+const alpha = [];
+const beta = [3, 4, 10];
+const delta = ['hey', 'there']
+
+//Create a const named aggregate which is an array containing alpha, beta and delta
+const aggregate = [alpha, beta, delta];
