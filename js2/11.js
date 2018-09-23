1be54aac6f2dc958f6c7d13f038859e489485f93
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-09-23 11:10:10 -0700

    #11 js2

---
+++ js2/11.js
@@ -0,0 +1,18 @@
+//After each step, log the value or question / answer.
+
+//Declare consts cats and aggregate with the values from #10
+const alpha = [];
+const beta = [3, 4, 10];
+const delta = ['hey', 'there']
+
+const aggregate = [alpha, beta, delta];
+
+const cats = ['Siamese', 'Calico', 'Maine Coon'];
+//Change the second value(Calico) in cats to Ragdoll
+
+
+//Using indexes, add a fourth value Burmese to cats
+
+//Using indexes, remove the first value from cats
+
+//Using indexes on the aggregate array, change the value of the second integer in beta to 37
